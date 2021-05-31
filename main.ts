05c9eb2a5d3970966d4776f0a0a5cffb5d56f9b3
Sady64 <83607769+Sady64@users.noreply.github.com>
2021-05-31 14:42:07 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let z3 = 0
+let z1 = 0
+let z2 = 0
+basic.forever(function () {
+    z3 = input.acceleration(Dimension.Z)
+    basic.pause(8)
+    z3 = input.acceleration(Dimension.Z)
+    basic.pause(8)
+    z3 = input.acceleration(Dimension.Z)
+    if (z1 < z2) {
+    	
+    }
+    if (z2 > z3) {
+    	
+    }
+})
